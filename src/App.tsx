3e4ee06a2106aaa04780cdff0b2a7506fb362921
Cenora6 <aleksandra.gasidlo@gmail.com>
2020-09-10 14:06:46 +0200

    Created animated tv, added style

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
+import AnimatedTv from "./components/animatedTv/AnimatedTv";
 
 function App() {
+
+    const [height, setHeight] = useState(0);
+    const [width, setWidth] = useState(0);
+
+    const updateWindowDimensions = (e: any) => {
+        const newWidth = e.clientX;
+        const newHeight = e.clientY;
+        setHeight(newHeight);
+        setWidth(newWidth);
+    };
+
   return (
-    <div className="App">
+    <div className="main" onMouseMove={updateWindowDimensions}>
       <p>works</p>
+        <AnimatedTv width={width} height={height}/>
     </div>
   );
 }
---
+++ src/components/animatedTv/AnimatedTv.tsx
@@ -0,0 +1,60 @@
+import React, {useState, useEffect} from 'react';
+
+export interface AnimatedTvProps {
+    width: any,
+    height: any
+}
+
+function AnimatedTv(props: AnimatedTvProps) {
+
+    const xCoordinates = props.width*100 / window.innerWidth+"%";
+    const yCoordinates = props.height*100 / window.innerHeight+"%";
+
+    const ballStyle = {
+        left: xCoordinates,
+        top: yCoordinates,
+        transform: "translate(-"+xCoordinates+",-"+yCoordinates+")"
+    };
+
+    return (
+        <section className='tv'>
+            <div className='tv__top'>
+                <div className='tv__top__left'></div>
+                <div className='tv__top__right'></div>
+            </div>
+            <div className='tv__background'>
+                <div className='tv__background__screen'>
+                    <div className='tv__background__screen__face'>
+                        <div className='tv__background__screen__face__eyes'>
+                            <div className='eyebrow-left'></div>
+                            <div className='tv__background__screen__face__eyes__left'>
+                                <div className='ball' style={ballStyle}></div>
+                            </div>
+                            <div className='eyebrow-right'></div>
+                            <div className='tv__background__screen__face__eyes__right'>
+                                <div className='ball' style={ballStyle}></div>
+                            </div>
+                        </div>
+                        <div className='tv__background__screen__face__mouth'></div>
+                    </div>
+                </div>
+            </div>
+            <div className='tv__buttons'>
+                <div className='tv__buttons__round'>
+                    <div className='tv__buttons__round__1'></div>
+                    <div className='tv__buttons__round__2'></div>
+                </div>
+                <div className='tv__buttons__lines'>
+                    <div className='line'></div>
+                    <div className='line'></div>
+                    <div className='line'></div>
+                    <div className='line'></div>
+                    <div className='line'></div>
+                    <div className='line'></div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default AnimatedTv;

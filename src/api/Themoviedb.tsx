0a8a1011e7a9b3306803627e27484849c8223590
Cenora6 <aleksandra.gasidlo@gmail.com>
2020-09-14 14:33:40 +0200

    Added buttons styling, added animation

--- src/api/Themoviedb.tsx
+++ src/api/Themoviedb.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import axios from "axios";
 const API_KEY = "7da3d91798b7102da10fe38896bae4fe";
 const url = "https://api.themoviedb.org/";
@@ -9,8 +8,9 @@ export function getAllGenres(setAllGenres: any, type: string) {
         .get<any>
         (`${url}3/genre/${type}/list?api_key=${API_KEY}&language=en-US`)
         .then(response => {
-            setAllGenres(response.data.genres)
-            // console.log(response.data)
+            setTimeout( () => {
+                setAllGenres(response.data.genres)
+            }, 500)
         })
         .catch(err => {
             console.log(err);
@@ -34,42 +34,21 @@ export function searchForRandom(setRandom: any, search: any, setActivePage: any)
             axios
                 .get<any>
                 (search.keyword ?
-                    `${url}3/discover/${search.type}?api_key=${API_KEY}&language=en-US&with_keywords=${search.keyword}&with_genres=${search.genre}`
+                    `${url}3/discover/${search.type}?api_key=${API_KEY}&language=en-US&with_keywords=${search.keyword}&with_genres=${search.genre}&page=${search.page}`
                 :
-                    `${url}3/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=${search.genre}`)
+                    `${url}3/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=${search.genre}&page=${search.page}`)
                 .then(response => {
                     const results: Array<any> = [];
                     response.data.results.forEach((data: any) => data.poster_path && results.push(data))
-                    let randomNumber = Math.floor(Math.random() * (results.length + 1));
+                    let randomNumber = Math.floor(Math.random() * (results.length));
                     setRandom(response.data.results[randomNumber])
                     setActivePage(2);
 
-                    console.log(response.data.results)
-                    console.log(`${url}3/discover/movie?api_key=${API_KEY}&language=en-US&with_keywords=${search.keyword}&with_genres=${search.genre}`)
+                    console.log("strona", search.page);
+                    console.log(search);
+                    console.log(response.data.results);
                 })
                 .catch(err => {
                     console.log(err);
                 });
 }
-
-    export function searchRandomPage(setRandom: any, search: any) {
-        if (search.type === "movie") {
-            console.log(search);
-
-            if (search.keyword) {
-                console.log("to jest film z keyword")
-                axios
-                    .get<any>
-                    (`${url}3/discover/movie?api_key=${API_KEY}&language=en-US&with_keywords=${search.keyword}&with_genres=${search.genre}`)
-                    .then(response => {
-                        console.log(response.data)
-                        console.log(response.data.total_pages)
-                        console.log(`${url}3/discover/movie?api_key=${API_KEY}&language=en-US&with_keywords=${search.keyword}&with_genres=${search.genre}`)
-                    })
-                    .catch(err => {
-                        console.log(err);
-                    });
-
-            }
-        }
-    }
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import {getAllGenres, getAllKeywords, searchForRandom} from "../../api/Themoviedb";
 
 interface searchData {
@@ -13,14 +13,16 @@ function Home() {
     const [allKeywords, setAllKeywords] = useState<any>();
     const [type, setType] = useState<any>('');
     const [genre, setGenre] = useState<any>('');
+    const [changeGenre, setChangeGenre] = useState<boolean>(false);
     const [keyword, setKeyword] = useState<string>('');
     const [keywordId, setKeywordId] = useState<string>('');
+    const [searchingPage, setSearchingPage] = useState<number>(1);
     const [activePage, setActivePage] = useState<any>(1);
     const [random, setRandom] = useState<any>();
 
     const changeType = (e: React.ChangeEvent<HTMLInputElement>) => {
         setType(e.target.value);
-        e.target.value && getAllGenres(setAllGenres, e.target.value);
+        getAllGenres(setAllGenres, e.target.value);
     }
 
     const changeKeywordInput = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -40,26 +42,54 @@ function Home() {
 
 
     const searchForMovie = (e: React.FormEvent) => {
-        setGenre('');
-        setType('');
-        setKeyword('');
-        setKeywordId('');
         e.preventDefault();
         const searchElement = {
             "type": type,
             "genre": genre,
-            "keyword": keywordId
+            "keyword": keywordId,
+            "page": searchingPage,
         };
 
         searchForRandom(setRandom, searchElement, setActivePage);
 
     }
 
-    const backToSearch = (e: React.MouseEvent) => {
+    const backToSearch = () => {
         console.log(type, genre, keywordId)
         setActivePage(1);
-        setRandom('');
+        setGenre('');
         setType('');
+        setKeyword('');
+        setKeywordId('');
+        setRandom('');
+        setSearchingPage(1);
+    }
+
+    const anotherSearch = (e: React.MouseEvent) => {
+        e.preventDefault();
+        setSearchingPage(searchingPage + 1)
+
+        const searchElement = {
+            "type": type,
+            "genre": genre,
+            "keyword": keywordId,
+            "page": searchingPage
+        };
+
+        searchForRandom(setRandom, searchElement, setActivePage);
+    }
+
+    const changeGenreAnimation = () => {
+        if (type === '') {
+            setTimeout( () => {
+                setChangeGenre(true);
+            }, 500)
+        } else {
+            setChangeGenre(false)
+            setTimeout( () => {
+                setChangeGenre(true)
+            }, 500)
+        }
     }
 
     return (
@@ -74,12 +104,12 @@ function Home() {
                     <div className='home__searchbox__type__radio'>
                         <div className='home__searchbox__type__radio__single single-box'>
                             <input type="radio" id='tv' name="type" value="tv" onChange={changeType}
-                                   checked={type === 'tv'}/>
+                                   checked={type === 'tv'} onClick={changeGenreAnimation}/>
                             <label htmlFor='tv'>tv show</label>
                         </div>
                         <div className='home__searchbox__types__radio__single single-box'>
                             <input type="radio" id='movie' name="type" value="movie" onChange={changeType}
-                                   checked={type === 'movie'}/>
+                                   checked={type === 'movie'} onClick={changeGenreAnimation}/>
                             <label htmlFor='movie'>movie</label>
                         </div>
                     </div>
@@ -88,7 +118,7 @@ function Home() {
                     <div className='home__searchbox__genres__title title-box'>
                         <h2>Genre</h2>
                     </div>
-                    <div className='home__searchbox__genres__radio'>
+                    <div className={`home__searchbox__genres__radio ${changeGenre ? 'show' : 'hide'} ${type === '' && 'hide'}`}>
                         {allGenres && allGenres.map( (single:any, index: number) => {
                             return (
                                 <div key={index} className='home__searchbox__genres__radio__single single-box'>
@@ -105,22 +135,25 @@ function Home() {
                         <h2>Keyword *</h2>
                     </div>
                     <div className='home__searchbox__keywords__input'>
-                        <input type="text" id='keyword' name="keyword" value={keyword} onChange={changeKeywordInput}/>
-                        <label htmlFor='keyword'></label>
-                        {allKeywords && <div className='home__searchbox__keywords__input__results'>
-                            {allKeywords.map((keyword: any) => {
+                        <label htmlFor='keyword'>
+                            <input type="text" id='keyword' name="keyword" value={keyword} onChange={changeKeywordInput}/>
+                        </label>
+                        <div className={`home__searchbox__keywords__input__results ${keyword ? "show" : "hide"}` }>
+                            {keyword !== '' && allKeywords && allKeywords.map((keyword: any) => {
                                 return (
                                     <span key={keyword.id} id={keyword.id} onClick={chooseKeyword}>{keyword.name}</span>
                                 )
                             })
                             }
                         </div>
-                        }
                     </div>
                 </div>
                 <div className='home__searchbox__submit'>
                     <div className='home__searchbox__submit__button'>
-                        <button type='submit'>Next >></button>
+                        <button type='submit'>
+                            Next
+                            <i className="fas fa-arrow-right"></i>
+                        </button>
                     </div>
                 </div>
             </form>
@@ -130,14 +163,29 @@ function Home() {
                 <div className='home__searchbox__result'>
                     {random ?
                         <>
-                            <img alt={random} src={`//image.tmdb.org/t/p/w220_and_h330_face/${random.poster_path}`}/>
-                            <h2>{random.original_title ? random.original_title : random.name}</h2>
-                            <button type='button' onClick={backToSearch}>Try again?</button>
+                            <div className='home__searchbox__result__details'>
+                                <img alt={random} src={`//image.tmdb.org/t/p/w220_and_h330_face/${random.poster_path}`}/>
+                                <div className='home__searchbox__result__details__info'>
+                                    <h2>{random.original_language === 'eng' ?
+                                        random.original_title ? random.original_title : random.name
+                                        :
+                                        random.title} {random.release_date}</h2>
+                                    <p>Score: {random.vote_average} ({random.vote_count} votes)</p>
+                                    <p>{random.overview}</p>
+                                </div>
+                            </div>
+
+                            <div className='home__searchbox__submit__button'>
+                                <button type='button' onClick={anotherSearch}>Try again?</button>
+                                <button type='button' onClick={backToSearch}>Change search?</button>
+                            </div>
                         </>
                         :
                         <>
                             <h2>No results... :(</h2>
-                            <button type='button' onClick={backToSearch}>Try again?</button>
+                            <div className='home__searchbox__submit__button'>
+                                <button type='button' onClick={backToSearch}>Try again?</button>
+                            </div>
                         </>
                     }
                 </div>

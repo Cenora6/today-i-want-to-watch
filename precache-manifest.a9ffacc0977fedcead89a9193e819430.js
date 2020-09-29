8dae30b4ecfe142e73d65f109d637d4aedeafa67
Cenora6 <aleksandra.gasidlo@gmail.com>
2020-09-29 14:02:33 +0200

    Updates

---
+++ precache-manifest.a9ffacc0977fedcead89a9193e819430.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c6d1c4ec6679894a075c60bef82b5ff",
+    "url": "/today-i-want-to-watch/index.html"
+  },
+  {
+    "revision": "0c999a210293a3fcfc0f",
+    "url": "/today-i-want-to-watch/static/css/main.d32cfec3.chunk.css"
+  },
+  {
+    "revision": "07e19bb81cb7279324ce",
+    "url": "/today-i-want-to-watch/static/js/2.4196d48c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/today-i-want-to-watch/static/js/2.4196d48c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c999a210293a3fcfc0f",
+    "url": "/today-i-want-to-watch/static/js/main.afef64ba.chunk.js"
+  },
+  {
+    "revision": "2f1d4276c61caedc6473",
+    "url": "/today-i-want-to-watch/static/js/runtime-main.b0c96ed0.js"
+  }
+]);
